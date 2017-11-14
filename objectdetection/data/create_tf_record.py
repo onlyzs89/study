c6f7af2320b8222798f40f6fb0a0fb2b7fbfe8fd
onlyzs <onlyzs1023@gmail.com>
2017-11-14 11:53:37 +0900

    Create create_tf_record.py

---
+++ objectdetection/data/create_tf_record.py
@@ -0,0 +1,142 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+BASE_DIR = "../models/research/"
+DATA_DIR = ""
+LABEL_MAP_PATH = "./label_map.pbtxt"
+OUT_DIR = ""
+
+import hashlib
+import io
+import logging
+import os
+import random
+import re
+import sys
+
+from lxml import etree
+import PIL.Image
+import tensorflow as tf
+
+sys.path.append(BASE_DIR)
+
+from object_detection.utils import dataset_util
+from object_detection.utils import label_map_util
+
+def get_class_name_from_filename(file_name):
+  match = re.match(r'([a-z]+)(_[0-9]+\.jpg)', file_name, re.I)
+  return match.groups()[0]
+
+def dict_to_tf_example(data, label_map_dict, image_subdirectory, ignore_difficult_instances=False):
+  img_path = os.path.join(image_subdirectory, data['filename'])
+  with tf.gfile.GFile(img_path, 'rb') as fid:
+    encoded_jpg = fid.read()
+  encoded_jpg_io = io.BytesIO(encoded_jpg)
+  image = PIL.Image.open(encoded_jpg_io)
+  if image.format != 'JPEG':
+    raise ValueError('Image format not JPEG')
+  key = hashlib.sha256(encoded_jpg).hexdigest()
+
+  width = int(data['size']['width'])
+  height = int(data['size']['height'])
+
+  xmin = []
+  ymin = []
+  xmax = []
+  ymax = []
+  classes = []
+  classes_text = []
+  truncated = []
+  poses = []
+  difficult_obj = []
+  for obj in data['object']:
+    difficult = bool(int(obj['difficult']))
+    if ignore_difficult_instances and difficult:
+      continue
+
+    difficult_obj.append(int(difficult))
+
+    xmin.append(float(obj['bndbox']['xmin']) / width)
+    ymin.append(float(obj['bndbox']['ymin']) / height)
+    xmax.append(float(obj['bndbox']['xmax']) / width)
+    ymax.append(float(obj['bndbox']['ymax']) / height)
+    class_name = get_class_name_from_filename(data['filename'])
+    classes_text.append(class_name.encode('utf8'))
+    classes.append(label_map_dict[class_name])
+    truncated.append(int(obj['truncated']))
+    poses.append(obj['pose'].encode('utf8'))
+
+  example = tf.train.Example(features=tf.train.Features(feature={
+      'image/height': dataset_util.int64_feature(height),
+      'image/width': dataset_util.int64_feature(width),
+      'image/filename': dataset_util.bytes_feature(
+          data['filename'].encode('utf8')),
+      'image/source_id': dataset_util.bytes_feature(
+          data['filename'].encode('utf8')),
+      'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+      'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+      'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
+      'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
+      'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
+      'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
+      'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
+      'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+      'image/object/class/label': dataset_util.int64_list_feature(classes),
+      'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
+      'image/object/truncated': dataset_util.int64_list_feature(truncated),
+      'image/object/view': dataset_util.bytes_list_feature(poses),
+  }))
+  return example
+
+def create_tf_record(output_filename, label_map_dict, annotations_dir, image_dir, examples):
+  writer = tf.python_io.TFRecordWriter(output_filename)
+  for idx, example in enumerate(examples):
+    path = os.path.join(annotations_dir, 'xmls', example + '.xml')
+    with tf.gfile.GFile(path, 'r') as fid:
+      xml_str = fid.read()
+    xml = etree.fromstring(xml_str)
+    data = dataset_util.recursive_parse_xml_to_dict(xml)['annotation']
+
+    tf_example = dict_to_tf_example(data, label_map_dict, image_dir)
+    writer.write(tf_example.SerializeToString())
+
+  writer.close()
+
+def main(_):
+  label_map_dict = label_map_util.get_label_map_dict(LABEL_MAP_PATH)
+  
+  image_dir = os.path.join(DATA_DIR, 'images')
+  annotations_dir = os.path.join(DATA_DIR, 'annotations')
+
+  train_output_path = os.path.join(OUT_DIR, 'train.record')
+  val_output_path = os.path.join(OUT_DIR, 'val.record')
+  
+  examples_path = os.path.join(annotations_dir, 'trainval.txt')
+  examples_list = dataset_util.read_examples_list(examples_path)
+  
+  random.seed(23)
+  random.shuffle(examples_list)
+  num_examples = len(examples_list)
+  num_train = int(0.7 * num_examples)
+  train_examples = examples_list[:num_train]
+  val_examples = examples_list[num_train:]
+  
+  create_tf_record(train_output_path, label_map_dict, annotations_dir,
+                   image_dir, train_examples)
+  create_tf_record(val_output_path, label_map_dict, annotations_dir,
+                   image_dir, val_examples)
+
+if __name__ == '__main__':
+  tf.app.run()
